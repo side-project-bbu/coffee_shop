b36e5d5f5d668f430118f540ed148c439fce26c9
Sabrey <limsabriey94@gmail.com>
2025-08-02 12:10:36 +0700

    update

---
+++ admin/src/components/UserProfile.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>This is user profile</p>
+</template>
+<script setup>
+// this is script
+</script>
+<style scoped>
+/* this is style */
+</style>
--- admin/src/components/header.vue
+++ admin/src/components/header.vue
@@ -1,11 +1,22 @@
 <template> 
-  <!-- Left side - Page title -->
-    <div class="flex flex-col items-center justify-center h-full text-center">
-      <h1 class="text-2xl font-semibold text-gray-800">{{ pageTitle }}</h1>
-    </div> 
+  <div class="relative flex items-center justify-between h-16 px-4 shadow">
+    <!-- Left spacer (invisible) -->
+    <div></div>
+
+    <!-- Center title (positioned absolutely) -->
+    <h1 class="absolute left-1/2 transform -translate-x-1/2 text-2xl font-semibold text-gray-800">
+      {{ pageTitle }}
+    </h1>
+
+    <!-- Right content -->
+    <UserProfile />
+  </div>
 </template>
 
+
+
 <script setup>
+import UserProfile from "@/components/UserProfile.vue"
 const props = defineProps({
   pageTitle: {
     type: String,
