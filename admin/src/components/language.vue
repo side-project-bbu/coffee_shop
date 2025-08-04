dcc934873ff9370b5444b7d726107ea7b972ae7f
Sabrey <limsabriey94@gmail.com>
2025-08-04 11:44:39 +0700

    update

--- admin/src/components/language.vue
+++ admin/src/components/language.vue
@@ -1,18 +1,49 @@
 <template>
-    <button type="button" @click="visible = true">
-            <i class="pi pi-globe mr-3"></i>
-            <span>Language</span>
-          </button>
+  <!-- Button to open the modal -->
+  <button type="button" @click="visible = true" >
+    <i class="pi pi-globe mr-4"></i>
+    <span>Language</span>
+  </button>
 
-          <Dialog v-model:visible="visible" modal header="Change Language" :style="{ width: '25rem' }">
-            <div  >
-                <Button type="button" label="English" severity="secondary" @click="visible = false"></Button></div>
-                <Button type="button" label="ភាសាខ្មែរ" @click="visible = false"></Button>
-            
-        </Dialog>
+  <!-- Centered modal/popover -->
+  <div
+    v-if="visible"
+    class="fixed inset-0 z-50 flex items-center justify-center  bg-opacity-50"
+  >
+    <div class="bg-white p-6 rounded shadow-md">
+  <div class="flex flex-col gap-4">
+    <p class="text-center text-black font-bold text-lg">Change Language</p>
+    
+    <!-- English -->
+    <div class="flex items-center gap-3">
+      <img src="/Images/united-kingdom.png" class="w-7 h-7" />
+      <button
+        type="button"
+        class="px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded"
+        @click="visible = false"
+      >
+        English
+      </button>
+    </div>
+
+    <!-- Khmer -->
+    <div class="flex items-center gap-3">
+      <img src="/Images/flag.png" class="w-7 h-7" />
+      <button
+        type="button"
+        class="px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded"
+        @click="visible = false"
+      >
+        ភាសាខ្មែរ
+      </button>
+    </div>
+  </div>
+</div>
+
+  </div>
 </template>
 <script setup>
 import { ref } from "vue";
+
 const visible = ref(false);
- 
 </script>
